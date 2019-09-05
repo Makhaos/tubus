660de8589dc2c460634ef7b0a2cc79d71e5b5743
Makhaos <manuel.soeiro7@gmail.com>
2019-09-05 11:32:29 +0200

    fixed algorithm to create 1 image per second

--- video_to_frames.py
+++ video_to_frames.py
@@ -2,24 +2,22 @@ import cv2
 import os
 
 
-def get_frame(video, sec):
-    video.set(cv2.CAP_PROP_POS_MSEC, sec * 1000)
+def get_frame(video, frame_rate, sec):
+    video.set(cv2.CAP_PROP_POS_FRAMES, frame_rate * sec)
     has_frames, image = video.read()
-    if has_frames:
-        cv2.imwrite("data/images/image" + str(sec) + ".jpg", image)  # save frame as JPG file
-        return has_frames
+    cv2.imwrite("data/images/image" + str(sec) + ".jpg", image)  # save frame as JPG file
 
 
 def main():
     # os.system("rm -r data/images/*")  # This deletes all images created
     video = cv2.VideoCapture("data/T20190821174253.AVI")
+    frame_rate = float(video.get(cv2.CAP_PROP_FPS))
+    number_of_frames = float(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    video_length = number_of_frames / frame_rate
     sec = 0
-    frame_rate = 1
-    success = get_frame(video, sec)
-    while success:
-        sec = sec + frame_rate
-        sec = round(sec, 2)
-        success = get_frame(video, sec)
+    while sec <= video_length:
+        get_frame(video, frame_rate, sec)
+        sec += 1
 
 
 if __name__ == "__main__":
