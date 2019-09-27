74b55a3638127b8f10ca09b3caf0b4995422fe8b
staffanbjorkdahl <staffan.bjorkdahl@essiq.se>
2019-09-27 15:57:14 +0200

    ordered stuff into methods for circle stuff

--- src/identify_circles_bit_map.py
+++ src/identify_circles_bit_map.py
@@ -3,13 +3,15 @@ import circle_fit as cf
 from matplotlib import pyplot as plt
 import numpy as np
 from src import identify_pixels
-from PIL import Image, ImageOps
-import collections
+from PIL import Image
+import os
+import time
+import sys
 
 
+start_time = time.time()
 
-radius_list = list()
-n_black_pixels_list = list()
+
 def yellow_detection(input_image):
     image_read = cv2.imread(input_image)
     hsv = cv2.cvtColor(image_read, cv2.COLOR_BGR2HSV)
@@ -29,6 +31,7 @@ def rgb_2_gray_scale(image_to_grayscale):
     return arr
 
 def count_pixels_in_circle(pos_and_radius,black_x, black_y, pixel_value):
+    n_black_pixels_list = list()
     black_pixels_pos = np.c_[black_x,black_y]
     print(black_pixels_pos.shape)
     for x, y, r in pos_and_radius:
@@ -37,92 +40,126 @@ def count_pixels_in_circle(pos_and_radius,black_x, black_y, pixel_value):
         n_black_pixels = np.count_nonzero(bool_array == True)
         n_black_pixels_list.append(n_black_pixels)
     return np.argmax(n_black_pixels_list)
+
+def create_grayscale_from_bitmap():
+    image_to_grayscale = cv2.imread('/home/staffanbjorkdahl/PycharmProjects/tubus/data/tempPicture.jpg')
+    arr = rgb_2_gray_scale(image_to_grayscale)
+    arr = arr[70:-70, 70:-70]
+    return arr
+
+def save_scatter_plot_and_get_pixels(arr,pixel_value):
+    indexArray_x, indexArray_y, indexArray_x_black, indexArray_y_black = identify_pixels.get_bright_and_dark_pixels(arr, pixel_value)
+    plt.close()
+    plt.scatter(indexArray_x_black, indexArray_y_black ,c = 'black')
+    plt.scatter(indexArray_x, indexArray_y, c = 'yellow')
+    plt.savefig('/home/staffanbjorkdahl/PycharmProjects/tubus/data/tempPicture2.jpg')
+    black_x = indexArray_x_black
+    black_y = indexArray_y_black
+    return black_x, black_y
+
+
+def get_the_edges():
+    raw_image = cv2.imread('/home/staffanbjorkdahl/PycharmProjects/tubus/data/tempPicture2.jpg')
+    bilateral_filtered_image = cv2.bilateralFilter(raw_image, 5, 175, 175)
+    #cv2.imshow('Bilateral', bilateral_filtered_image)
+    #cv2.waitKey(0)
+    edge_detected_image = cv2.Canny(bilateral_filtered_image, 75, 200)
+    #cv2.imshow('Edge', edge_detected_image)
+    #cv2.waitKey(0)
+    contours, _ = cv2.findContours(edge_detected_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
+
+def put_contours_in_list(contours):
+    contour_list = []
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True), True)
+        area = cv2.contourArea(contour)
+        if ((len(approx) > 8) & (area > 25) ):
+            contour_list.append(contour)
+    updated_contour_list = []
+    for item in contour_list:
+        if item.shape[0] > 30:
+            updated_contour_list.append(item)
+    return updated_contour_list
+
+
+
+def winnow_radii(updated_contour_list):
+    radius_list = list()
+    for item in updated_contour_list:
+        xc, yc, r, _ = cf.least_squares_circle(item[:, 0])
+        if r > 30 and r < 200:
+            radius_list.append([xc, yc, r])
+    return radius_list
+
+def flip_orig_image(input_image):
+    orig_image_read = plt.imread(input_image)
+    plt.close()
+    plt.imshow(orig_image_read)
+    plt.savefig('/home/staffanbjorkdahl/PycharmProjects/tubus/data/tempPicture3.jpg')
+    orig_image = plt.imread('/home/staffanbjorkdahl/PycharmProjects/tubus/data/tempPicture3.jpg')
+    plt.close()
+    orig_flipped = np.flipud(orig_image)
+    return orig_flipped
+
+
+def plot_circles_on_orig(index_most_black_pixels,pos_and_radius,input_image):
+    number_of_points = np.linspace(0, 2*np.pi, 200)
+    xunit = pos_and_radius[index_most_black_pixels][2]*np.cos(number_of_points) + pos_and_radius[index_most_black_pixels][0]
+    yunit = pos_and_radius[index_most_black_pixels][2]*np.sin(number_of_points) + pos_and_radius[index_most_black_pixels][1]
+    #plt.close()
+    orig_flipped = flip_orig_image(input_image)
+    plt.imshow(orig_flipped)
+    plt.plot(xunit,yunit)
+    plt.axis('scaled')
+    folder_name = input_image.split('/')[-2]
+    image_name = input_image.split('/')[-1]
+    print('/home/staffanbjorkdahl/PycharmProjects/tubus/data/circle_folder/' + image_name)
+    plt.savefig('/home/staffanbjorkdahl/PycharmProjects/tubus/data/circle_folder/' + image_name)
+    plt.show()
+
+def remove_temp_jpgs():
+    os.remove('/home/staffanbjorkdahl/PycharmProjects/tubus/data/tempPicture.jpg')
+    os.remove('/home/staffanbjorkdahl/PycharmProjects/tubus/data/tempPicture2.jpg')
+    os.remove('/home/staffanbjorkdahl/PycharmProjects/tubus/data/tempPicture3.jpg')
+
+
 #input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190823155414/image53.jpg'
 #input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190829141432/image62.jpg'
 #input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190823152643/image112.jpg'
 #input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190829140453/image172.jpg'
 #input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190829140453/image164.jpg'
-input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190829140453/image29.jpg'
+#input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190829140453/image29.jpg'
 #input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190823154915/image19.jpg'
 #input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190823154915/image68.jpg' # No hole
 #input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190823154915/image105.jpg' # No hole
-yellow_detection(input_image)
-image_to_grayscale = cv2.imread('/home/staffanbjorkdahl/PycharmProjects/tubus/data/tempPicture.jpg')
-arr = rgb_2_gray_scale(image_to_grayscale)
-arr = arr[70:-70, 70:-70]
-
-plt.close()
 pixel_value = 20
-indexArray_x, indexArray_y, indexArray_x_black, indexArray_y_black = identify_pixels.get_bright_and_dark_pixels(arr, pixel_value)
-plt.scatter(indexArray_x_black, indexArray_y_black ,c = 'black')
-plt.scatter(indexArray_x, indexArray_y, c = 'yellow')
-plt.savefig('/home/staffanbjorkdahl/PycharmProjects/tubus/data/tempPicture2.jpg')
 
+def main(path_to_framefolder, number_of_images_in_folder):
+    #for i in range(103, number_of_images_in_folder+1):
+    #input_image = path_to_framefolder+'/image'+str(i)+'.jpg'
+    input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190829140453/image29.jpg'
+    input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190823154915/image19.jpg'
+    input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190829140453/image164.jpg'
+    input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190823152643/image112.jpg'
+    input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190829140453/image172.jpg'
+    input_image = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190829141432/image62.jpg'
+    yellow_detection(input_image)
+    arr = create_grayscale_from_bitmap()
+    black_x, black_y=save_scatter_plot_and_get_pixels(arr,pixel_value)
+    contours = get_the_edges()
+    updated_contour_list = put_contours_in_list(contours)
+    pos_and_radius = winnow_radii(updated_contour_list)
+    index_most_black_pixels = count_pixels_in_circle(pos_and_radius,black_x, black_y, pixel_value)
+    plot_circles_on_orig(index_most_black_pixels,pos_and_radius,input_image)
+    remove_temp_jpgs()
+    plt.close()
 
-raw_image = cv2.imread('/home/staffanbjorkdahl/PycharmProjects/tubus/data/tempPicture2.jpg')
-#cv2.imshow('Original Image', raw_image)
-#cv2.waitKey(0)
+path_to_framefolder = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190823154915'
+number_of_images_in_folder = 239
+main(path_to_framefolder,number_of_images_in_folder)
 
+end_time = time.time()-start_time
 
-bilateral_filtered_image = cv2.bilateralFilter(raw_image, 5, 175, 175)
-#cv2.imshow('Bilateral', bilateral_filtered_image)
-#cv2.waitKey(0)
-
-edge_detected_image = cv2.Canny(bilateral_filtered_image, 75, 200)
-#cv2.imshow('Edge', edge_detected_image)
-#cv2.waitKey(0)
-
-contours, _ = cv2.findContours(edge_detected_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-contour_list = []
-for contour in contours:
-    approx = cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True), True)
-    area = cv2.contourArea(contour)
-    if ((len(approx) > 8) & (area > 25) ):
-        contour_list.append(contour)
-
-updated_contour_list = []
-for item in contour_list:
-    if item.shape[0] > 30:
-        updated_contour_list.append(item)
-
-
-#cv2.drawContours(raw_image, updated_contour_list,  -1, (255,0,0), 2)
-#cv2.imshow('Objects Detected',raw_image)
-#cv2.waitKey(0)
-
-print(updated_contour_list)
-c = 1
-for item in updated_contour_list:
-    xc, yc, r, _ = cf.least_squares_circle(item[:, 0])
-    print(r)
-    if r > 30 and r < 200:
-
-        radius_list.append([xc, yc, r])
-        c += 1
-
-pos_and_radius = radius_list
-black_x = indexArray_x_black
-black_y = indexArray_y_black
-index_most_black_pixels = count_pixels_in_circle(pos_and_radius,black_x, black_y, pixel_value)
-plt.close()
-orig_image_read = plt.imread(input_image)
-plt.imshow(orig_image_read)
-orig_image = plt.savefig('/home/staffanbjorkdahl/PycharmProjects/tubus/data/tempPicture3.jpg')
-
-orig_image = plt.imread('/home/staffanbjorkdahl/PycharmProjects/tubus/data/tempPicture3.jpg')
-orig_flipped = np.flipud(orig_image)
-plt.close()
-print(radius_list)
-number_of_points = np.linspace(0, 2*np.pi, 200)
-print(pos_and_radius[index_most_black_pixels][2])
-
-xunit = pos_and_radius[index_most_black_pixels][2]*np.cos(number_of_points) + pos_and_radius[index_most_black_pixels][0]
-yunit = pos_and_radius[index_most_black_pixels][2]*np.sin(number_of_points) + pos_and_radius[index_most_black_pixels][1]
-#im = plt.imread(raw_image)
-plt.close()
-plt.imshow(orig_flipped)
-plt.plot(xunit,yunit)
-plt.axis('scaled')
-plt.show()
-
+print(end_time)
