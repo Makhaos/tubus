a1e116815128742a8d6d4bfddf2f8d2472a30b07
Makhaos <manuel.soeiro7@gmail.com>
2019-10-01 11:36:12 +0200

    fixed loop of circle identification. added csv creation for circle center

--- src/identify_circles_bit_map.py
+++ src/identify_circles_bit_map.py
@@ -11,6 +11,7 @@ from src import color_detection
 
 start_time = time.time()
 root = utils.get_project_root()
+pos_and_radius_list = list()
 
 
 # def yellow_detection(input_image):
@@ -33,13 +34,16 @@ def rgb_2_gray_scale(image_to_grayscale):
 def count_pixels_in_circle(pos_and_radius, black_x, black_y):
     n_black_pixels_list = list()
     black_pixels_pos = np.c_[black_x, black_y]
+    pixel = True
     for x, y, r in pos_and_radius:
         dist_sqrd = (black_x - x) ** 2 + (black_y - y) ** 2
         bool_array = dist_sqrd <= r ** 2
         n_black_pixels = np.count_nonzero(bool_array == True)
         n_black_pixels_list.append(n_black_pixels)
+        print('pixels', n_black_pixels)
     if not n_black_pixels_list:
-        return 0
+        pixel = False
+        return pixel
     else:
         return np.argmax(n_black_pixels_list)
 
@@ -94,7 +98,7 @@ def winnow_radii(updated_contour_list):
     radius_list = list()
     for item in updated_contour_list:
         xc, yc, r, _ = cf.least_squares_circle(item[:, 0])
-        if 30 < r < 200:
+        if 30 < r < 150:
             radius_list.append([xc, yc, r])
     return radius_list
 
@@ -102,16 +106,16 @@ def winnow_radii(updated_contour_list):
 def flip_orig_image(input_image):
     orig_image_read = plt.imread(input_image)
     plt.close()
-    plt.imshow(orig_image_read)
-    plt.savefig(str(root) + '/data/tempPicture3.jpg')
-    orig_image = plt.imread(str(root) + '/data/tempPicture3.jpg')
-    plt.close()
-    orig_flipped = np.flipud(orig_image)
+    # plt.imshow(orig_image_read)
+    # plt.savefig(str(root) + '/data/tempPicture3.jpg')
+    # orig_image = plt.imread(str(root) + '/data/tempPicture3.jpg')
+    # plt.close()
+    orig_flipped = np.flipud(orig_image_read)
     return orig_flipped
 
 
 def plot_circles_on_orig(index_most_black_pixels, pos_and_radius, input_image):
-    if index_most_black_pixels != 0:
+    if index_most_black_pixels is not False:
         number_of_points = np.linspace(0, 2 * np.pi, 200)
         xunit = pos_and_radius[index_most_black_pixels][2] * np.cos(number_of_points) + \
                 pos_and_radius[index_most_black_pixels][0]
@@ -128,12 +132,22 @@ def plot_circles_on_orig(index_most_black_pixels, pos_and_radius, input_image):
         print(str(root) + '/data/circle_folder/' + image_name)
         plt.savefig(str(root) + '/data/circle_folder/' + image_name)
         # plt.show()
+        return pos_and_radius
     else:
         orig = plt.imread(input_image)
         plt.close()
+        plt.text(300, 50, "No circle found", size=20, rotation=20.,
+                 ha="center", va="center",
+                 bbox=dict(boxstyle="round",
+                           ec=(1., 0.5, 0.5),
+                           fc=(1., 0.8, 0.8),
+                           )
+                 )
         plt.imshow(orig)
         image_name = input_image.split('/')[-1]
         plt.savefig(str(root) + '/data/circle_folder/' + image_name)
+        return 0
+
 
 
 
@@ -158,6 +172,8 @@ def main(images_folder, images_folder_res):
     # for i in range(103, number_of_images_in_folder+1):
     # input_image = path_to_framefolder+'/image'+str(i)+'.jpg'
     pixel_value = 20
+    pos_and_radius_list = list()
+    index_of_variance_data = 0
     for image_name, image_path in utils.folder_reader(images_folder_res):
         print(image_name)
         # input_image = str(root) + '/data/frames/T20190829141432/image62.jpg'
@@ -170,7 +186,10 @@ def main(images_folder, images_folder_res):
         updated_contour_list = put_contours_in_list(contours)
         pos_and_radius = winnow_radii(updated_contour_list)
         index_most_black_pixels = count_pixels_in_circle(pos_and_radius, black_x, black_y)
-        plot_circles_on_orig(index_most_black_pixels, pos_and_radius, images_folder + '/' + image_name) # TODO need original image
+        position = plot_circles_on_orig(index_most_black_pixels, pos_and_radius, images_folder + '/' + image_name)
+        pos_and_radius_list.append(position)
+        index_of_variance_data = index_of_variance_data + 1
+        identify_pixels.write_plot_data_2_csv(position, images_folder, index_of_variance_data)
         # remove_temp_jpgs()
         plt.close()
 
--- src/main.py
+++ src/main.py
@@ -17,32 +17,32 @@ def main():
     for video_name, video_path in utils.folder_reader(videos_folder):
         video_name_no_extension, video_name_extension = os.path.splitext(video_name)
 
-        # Create frames from a video
-        os.makedirs(frames_folder + '/' + video_name_no_extension, exist_ok=True)
-        video_to_frames.get_frame(video_path, video_name, frames_folder, video_name_no_extension)
-
-        # TODO improve blurriness algorithm and code
-        # # Calculate blurriness of each image
-        # fm_list = blur.calculate_laplacian(video_name_no_extension, frames_folder)
-        # # Print blurriness results
-        # blur.print_blur_results(fm_list, video_name_no_extension)
-
-        # Detect yellow in frames
-        hue_low = 18
-        saturation_low = 25
-        value_low = 25
-        color_detection.yellow_detection(frames_folder + '/' + video_name_no_extension,
-                                         frames_folder + '/res/' + video_name_no_extension, hue_low,
-                                         saturation_low, value_low)
-
-        # Calculate variance
-        variance_list = identify_pixels.main(frames_folder + '/res/' + video_name_no_extension)
-
-        # Plot variance results
-        plot.plot_list(variance_list, video_name_no_extension)  # TODO improve quality of saved plots
-
-        # Identify black holes (circles) in the pipes
-        hue_low = 16
+        # # Create frames from a video
+        # os.makedirs(frames_folder + '/' + video_name_no_extension, exist_ok=True)
+        # video_to_frames.get_frame(video_path, video_name, frames_folder, video_name_no_extension)
+        #
+        # # TODO improve blurriness algorithm and code
+        # # # Calculate blurriness of each image
+        # # fm_list = blur.calculate_laplacian(video_name_no_extension, frames_folder)
+        # # # Print blurriness results
+        # # blur.print_blur_results(fm_list, video_name_no_extension)
+        #
+        # # Detect yellow in frames
+        # # hue_low = 18
+        # # saturation_low = 25
+        # # value_low = 25
+        # # color_detection.yellow_detection(frames_folder + '/' + video_name_no_extension,
+        # #                                  frames_folder + '/res/' + video_name_no_extension, hue_low,
+        # #                                  saturation_low, value_low)
+        # #
+        # # # Calculate variance
+        # # variance_list = identify_pixels.main(frames_folder + '/res/' + video_name_no_extension)
+        # #
+        # # # Plot variance results
+        # # plot.plot_list(variance_list, video_name_no_extension)  # TODO improve quality of saved plots
+        #
+        # # Identify black holes (circles) in the pipes
+        hue_low = 15
         saturation_low = 25
         value_low = 0
         color_detection.yellow_detection(frames_folder + '/' + video_name_no_extension,
