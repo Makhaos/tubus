45516a04173f008bc4ad145f357c51ba781b4590
Makhaos <manuel.soeiro7@gmail.com>
2019-10-07 14:34:53 +0200

    updated color_detection module. New class named ColorDetector

--- src/color_detection.py
+++ src/color_detection.py
@@ -2,23 +2,35 @@ import cv2
 import numpy as np
 import utils
 import os
-from matplotlib import pyplot as plt
 
 
-def yellow_detection(images_folder, filtered_images_folder, h_low, s_low, v_low):
-    bit_map_list = list()
-    os.makedirs(filtered_images_folder, exist_ok=True)
-    for image_name, image_path in utils.folder_reader(images_folder):
-        print(image_name)
-        image_read = cv2.imread(image_path + '/' + image_name)
-        hsv = cv2.cvtColor(image_read, cv2.COLOR_BGR2HSV)
-        yellow_lower = np.array([h_low, s_low, v_low], np.uint8)
-        yellow_upper = np.array([30, 255, 255], np.uint8)
-        yellow = cv2.inRange(hsv, yellow_lower, yellow_upper)
-        res = cv2.bitwise_and(src1=image_read, src2=image_read, mask=yellow)
-        cv2.imwrite(filtered_images_folder + '/' + image_name, res)
-        bit_map_list.append(res)
-        # plt.imshow(res)
-        # plt.savefig(filtered_images_folder + '/' + image_name)
-    return bit_map_list
+class ColorDetector:
+    def __init__(self, frames_folder, output_frames_folder):
+        self.frames_folder = frames_folder
+        self.output_images_folder = output_frames_folder
 
+    def detect_yellow(self, hsv_low, hsv_high):
+        os.makedirs(self.output_images_folder, exist_ok=True)
+        for image_name, image_path in utils.folder_reader(self.frames_folder):
+            image_read = cv2.imread(image_path + '/' + image_name)
+            hsv = cv2.cvtColor(image_read, cv2.COLOR_BGR2HSV)
+            yellow_lower = np.array(hsv_low, np.uint8)
+            yellow_upper = np.array(hsv_high, np.uint8)
+            yellow = cv2.inRange(hsv, yellow_lower, yellow_upper)
+            res = cv2.bitwise_and(src1=image_read, src2=image_read, mask=yellow)
+            cv2.imwrite(self.output_images_folder + '/' + image_name, res)
+
+
+def main():
+    root = utils.get_project_root()
+    frames_folder = str(root) + '/data/frames'
+    output_frames_folder = str(root) + '/data/frames_res'
+    color_detector = ColorDetector(frames_folder, output_frames_folder)
+    # HSV values
+    yellow_low = [18, 25, 25]
+    yellow_high = [30, 255, 255]
+    color_detector.detect_yellow(yellow_low, yellow_high)
+
+
+if __name__ == '__main__':
+    main()
