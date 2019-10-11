1f9730038ba6ab1861d964c91184eb0fd8f380e0
staffanbjorkdahl <staffan.bjorkdahl@essiq.se>
2019-10-11 13:57:15 +0200

    Friday stuff

--- src/identify_circles_bit_map.py
+++ src/identify_circles_bit_map.py
@@ -6,38 +6,156 @@ from src import identify_pixels
 from PIL import Image
 import os
 import time
-import utils
-import color_detection
-
-
+import common.utils as utils
+import src.color_detection as cd
 
 start_time = time.time()
 root = utils.get_project_root()
 pos_and_radius_list = list()
 
 
-class Images:
+class Images():
     def __init__(self, image_folder_name, res_image_folder_name):
         self.image_folder_name = image_folder_name
         self.res_image_folder_name = res_image_folder_name
-
-
+    pixel_value = 20
 
 class ScatterImages(Images):
 
-    def get_res(self,yellow_low,yellow_high):
-        color_detection.ColorDetector(self.image_folder_name, self.res_image_folder_name).detect_yellow(yellow_low, yellow_high)
+    scatter_image_folder_name = str()
+    index_array_x_black = list()
+    index_array_y_black = list()
 
-    def get_grayscale_images(self):
-        #res_image_folder_name_list = (self.res_image_folder_name.split('/')[:-1]) + ['grayscale']
-        res_image_folder_name_list = self.res_image_folder_name + 'grayscale'
-        grayscale_image_folder_name = os.path.join(res_image_folder_name_list) # OK with / here?
-        os.makedirs(grayscale_image_folder_name, exist_ok=True)
+    def get_res(self, yellow_low, yellow_high):
+        cd.ColorDetector(self.image_folder_name, self.res_image_folder_name).detect_yellow(yellow_low, yellow_high)
+
+    def get_scatter_plot(self):
+        scatter_image_folder_name = os.path.join(self.res_image_folder_name + 'scatter')
+        ScatterImages.scatter_image_folder_name = scatter_image_folder_name
+        os.makedirs(scatter_image_folder_name, exist_ok=True)
+        c = 0
         for image_name, image_path in utils.folder_reader(self.res_image_folder_name):
             image_read = cv2.imread(os.path.join(image_path, image_name))
-            grayscale_image = Image.fromarray(image_read).convert("L")
-            grayscale_image_array = np.asarray(grayscale_image)
-            cv2.imwrite(os.path.join(grayscale_image_folder_name, image_name), grayscale_image_array)
+            img = Image.fromarray(image_read).convert("L")
+            grayscale_image_array = np.asarray(img)
+            index_array_x_yellow, index_array_y_yellow, index_array_x_black, index_array_y_black = identify_pixels.get_bright_and_dark_pixels(
+                grayscale_image_array,
+                Images.pixel_value)
+            plt.scatter(index_array_x_black, index_array_y_black, c='red', marker=10)
+            plt.savefig(os.path.join(scatter_image_folder_name, image_name))
+            plt.close()
+            c += 1
+            print(c)
+
+        return scatter_image_folder_name
+
+class EdgeDetection(Images):
+    contour_list = list()
+    def get_edges(self):
+        scatter_image_folder_name = ScatterImages.scatter_image_folder_name
+        print(scatter_image_folder_name)
+        for image_name, image_path in utils.folder_reader(scatter_image_folder_name):
+            raw_image = cv2.imread(os.path.join(image_path, image_name))
+            bilateral_filtered_image = cv2.bilateralFilter(raw_image, 5, 175, 175)
+            edge_detected_image = cv2.Canny(bilateral_filtered_image, 75, 200)
+            self.contours, _ = cv2.findContours(edge_detected_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    def get_contours(self):
+        contour_list = []
+        for contour in self.contours:
+            approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
+            area = cv2.contourArea(contour)
+            if len(approx) > 8 and area > 25:
+                contour_list.append(contour)
+
+        updated_contour_list = []
+        for item in contour_list:
+            if item.shape[0] > 30:
+                updated_contour_list.append(item)
+        EdgeDetection.contour_list = updated_contour_list
+
+
+class CirclePosition(Images):
+
+    index_for_most_black_pixel = int()
+    def get_valid_radii(self):
+        cont = EdgeDetection.contour_list
+        radius_list = list()
+        for item in cont:
+            x_center, y_center, radius, _ = cf.least_squares_circle(item[:, 0])
+            if 30 < radius < 150:
+                radius_list.append([x_center, y_center, radius])
+        self.position_and_radius = radius_list
+        return self.position_and_radius
+
+
+    ## HÄR ÄR JAG NÄR JAG GÅR HEM PÅ FREDAG
+    def count_pixels_in_circle(self, black_x_position, black_y_position):
+        n_black_pixels_list = list()
+        for x_center, y_center, radius in self.position_and_radius:
+            dist_sqrd = (black_x_position - x_center) ** 2 + (black_y_position - y_center) ** 2
+            bool_array = dist_sqrd <= radius ** 2
+            n_black_pixels = np.count_nonzero(bool_array == True)
+            n_black_pixels_list.append(n_black_pixels)
+            print('pixels', n_black_pixels)
+        if not n_black_pixels_list:
+            CirclePosition.index_for_most_black_pixel = False
+        else:
+            CirclePosition.index_for_most_black_pixel = np.argmax(n_black_pixels_list)
+
+
+    def flip_orig_image(self):
+        orig_image_read = cv2.imread(self)
+        plt.close()
+        orig_flipped = np.flipud(orig_image_read)
+        return orig_flipped
+
+
+
+image_folder_name = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/tempfile'
+res_image_folder_name = os.path.join(str(root), 'data', 'frames', 'T20190823155414', 'res')
+
+
+scatter_images = ScatterImages(image_folder_name, res_image_folder_name)
+yellow_low = [14, 25, 25]
+yellow_high = [30, 255, 255]
+scatter_images.get_res(yellow_low, yellow_high)
+scatter_images.get_scatter_plot()
+
+edge_detection = EdgeDetection(image_folder_name, res_image_folder_name)
+edge_detection.get_edges()
+circle = CirclePosition(image_folder_name, res_image_folder_name)
+circle.get_valid_radii()
+# circle.count_pixels_in_circle()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def rgb_2_gray_scale(image_to_grayscale):
@@ -71,8 +189,9 @@ def create_grayscale_from_bitmap(image_path_name):
 
 
 def save_scatter_plot_and_get_pixels(arr, pixel_value, image_path, image_name):
-    index_array_x_yellow, index_array_y_yellow, index_array_x_black, index_array_y_black = identify_pixels.get_bright_and_dark_pixels(arr,
-                                                                                                             pixel_value)
+    index_array_x_yellow, index_array_y_yellow, index_array_x_black, index_array_y_black = identify_pixels.get_bright_and_dark_pixels(
+        arr,
+        pixel_value)
     os.makedirs(image_path, exist_ok=True)
     plt.close()
     plt.scatter(index_array_x_black, index_array_y_black, c='black')
@@ -142,7 +261,6 @@ def plot_circles_on_orig(index_most_black_pixels, pos_and_radius, input_image):
     else:
         plt.close()
 
-        # plt.imshow(orig)
         orig_flipped = flip_orig_image(input_image)
         plt.imshow(orig_flipped)
         plt.text(300, 50, "No circle found", size=20, rotation=20.,
@@ -200,19 +318,4 @@ def main(images_folder, images_folder_res):
         identify_pixels.write_plot_data_2_csv(distance_from_center, images_folder, index_of_variance_data)
         plt.close()
 
-
-
-
-
-#main(images_folder,images_folder_res)
-
-image_folder_name = '/home/staffanbjorkdahl/PycharmProjects/tubus/data/frames/T20190823155414'
-res_image_folder_name = os.path.join(str(root), 'data', 'frames', 'T20190823155414', 'res')
-print(res_image_folder_name)
-scatter_images = ScatterImages(image_folder_name, res_image_folder_name)
-
-yellow_low = [18, 25, 25]
-yellow_high = [30, 255, 255]
-scatter_images.get_res(yellow_low, yellow_high)
-scatter_images.get_grayscale_images()
-print(scatter_images)
+# main(image_folder_name,res_image_folder_name)
