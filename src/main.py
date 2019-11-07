e945ebe4c6de96d1b1f1b2a65c0f9f67a1f7de41
Manuel <manuel.soeiro7@gmail.com>
2019-11-07 16:07:13 +0100

    uncomment circles

--- src/main.py
+++ src/main.py
@@ -27,29 +27,29 @@ def main():
         blur_detector.calculate_laplacian()
         blur_detector.blur_results()
         # Identify pixels
-        # variance_index = 0
-        # for image_name, image_directory in utils.folder_reader(frames_raw):
-        #     variance_index += 1
-        #     image_name_path = os.path.join(image_directory, image_name)
-        #     variance_image = identify_pixels.Variance(image_name_path)
-        #     variance_image.create_grayscale_from_rgb()
-        #     variance_image.get_bright_and_dark_pixels()
-        #     variance_image.calculate_variance()
-        #     csv = identify_pixels.WritingCSV(image_name_path)
-        #     csv.write_plot_data_2_csv(variance_index)
-        # # Identify circles
-        # yellow_low = [14, 25, 25]
-        # yellow_high = [30, 255, 255]
-        # scatter_images = identify_circles.ScatterImages(video_name_no_extension)
-        # scatter_images.get_res(yellow_low, yellow_high)
-        # scatter_images.get_scatter_plot()
-        # edge_detection = identify_circles.EdgeDetection(video_name_no_extension)
-        # edge_detection.get_edges()
-        # circle_pos = identify_circles.CirclePosition(video_name_no_extension)
-        # circle_pos.get_valid_radii()
-        # circle_pos.count_pixels_in_circle()
-        # circle_pos.plot_circles_on_raw_image()
-        # identify_circles.CirclePosition.features_selected_circle = list()
+        variance_index = 0
+        for image_name, image_directory in utils.folder_reader(frames_raw):
+            variance_index += 1
+            image_name_path = os.path.join(image_directory, image_name)
+            variance_image = identify_pixels.Variance(image_name_path)
+            variance_image.create_grayscale_from_rgb()
+            variance_image.get_bright_and_dark_pixels()
+            variance_image.calculate_variance()
+            csv = identify_pixels.WritingCSV(image_name_path)
+            csv.write_plot_data_2_csv(variance_index)
+        # Identify circles
+        yellow_low = [14, 25, 25]
+        yellow_high = [30, 255, 255]
+        scatter_images = identify_circles.ScatterImages(video_name_no_extension)
+        scatter_images.get_res(yellow_low, yellow_high)
+        scatter_images.get_scatter_plot()
+        edge_detection = identify_circles.EdgeDetection(video_name_no_extension)
+        edge_detection.get_edges()
+        circle_pos = identify_circles.CirclePosition(video_name_no_extension)
+        circle_pos.get_valid_radii()
+        circle_pos.count_pixels_in_circle()
+        circle_pos.plot_circles_on_raw_image()
+        identify_circles.CirclePosition.features_selected_circle = list()
 
 
 if __name__ == "__main__":
